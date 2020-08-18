d75af83807a3e8490c83b65bd714d2b047199791
ak6130 <albert.polletta@gmail.com>
2020-08-18 12:29:14 +0200

    Update basic-page.php

--- site/templates/basic-page.php
+++ site/templates/basic-page.php
@@ -6,7 +6,7 @@
 		<link rel="stylesheet" type="text/css" href="<?php echo $config->urls->templates?>styles/main.css" />
 	</head>
 	<body>
-		<img src="<?php $page->flyer1->url ?>" alt="Flyer Seite 1" width="100%">
-		<img src="<?php $page->flyer2->url ?>" alt="Flyer Seite 2" width="100%">	
+		<img src="<?php echo $page->flyer1->url; ?>" alt="Flyer Seite 1" width="100%">
+		<img src="<?php echo $page->flyer2->url; ?>" alt="Flyer Seite 2" width="100%">	
 	</body>
 </html>
